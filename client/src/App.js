2ec596ac971825dcf391b3560bf8b345168c9a6b
Mike Tian <rtian0627@gmail.com>
2024-11-10 13:33:11 -0800

    Normalize line endings

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <h1>Welcome to My React App</h1>
       <RandomQuote />
+      <h1><button><a href='second_page.html'>Drop my exam schedule</a></button></h1>
     </div>
-  );
+  );  
 }
 
 export default App;
