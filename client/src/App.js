70a4d6cf7eb3f6a0d0902772e4c029c8f2157ef5
Jessie Zhou <jzhou0819@gmail.com>
2024-11-10 10:53:46 -0800

    Remove submodule and add client folder as part of main repo

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
